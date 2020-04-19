0125226451e3d6216f4fb59fcb328f6485631057
Jor Bashllari <altnate2000@gmail.com>
2020-04-19 04:17:37 +0200

    start work on command parsing

--- src/cmds/cmd_quit.c
+++ src/cmds/cmd_quit.c
@@ -11,23 +11,6 @@
 #include "cmds.h"
 #include "helpers.h"
 
-static void getcmd(char *buf, cmdstr_t *);
-
-void handle_cmd(int sock, char *buf, client_t *c)
-{
-    cmdstr_t cmd = {0};
-
-    getcmd(buf, &cmd);
-    (void)sock;
-    (void)c;
-}
-
-static void getcmd(char *buf, cmdstr_t *to)
-{
-    (void)buf;
-    (void)to;
-}
-
 void cmd_quit(client_t *c, char *buf)
 {
     memset(buf, 0, MAXBUFLEN);
