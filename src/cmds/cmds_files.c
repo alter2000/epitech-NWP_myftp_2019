6ed34a552ae86c5446701100de9fcdc778ebb4b3
Jor Bashllari <altnate2000@gmail.com>
2020-04-19 23:23:35 +0200

    add cmd_dele + list

---
+++ src/cmds/cmds_files.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myftp_2019
+** File description:
+** automated desc ftw
+*/
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include "helpers.h"
+#include "cmds.h"
+
+void cmd_dele(client_t *c, char *buf)
+{
+    if (remove(buf)) {
+        msgsend(c->f.fd, 450, strerror(errno));
+        return;
+    }
+    msgsend(c->f.fd, 250, "");
+}
+
+void cmd_list(client_t *c, char *buf)
+{
+}
